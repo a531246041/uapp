46965aaf695aa2c115ce3583bec4dd971e69926c
a531246041 <33302476+a531246041@users.noreply.github.com>
2019-01-09 11:55:42 +0800

    Add files via upload

---
+++ TestDemo/pages/le-mpvueGestureLock/le-mpvueGestureLock.vue
@@ -0,0 +1,33 @@
+<template>
+	<view>
+		<MyTest containerWidth="850" cWidth="650" cycleRadius="40" v-on:end="endfuntion" :password="password"></MyTest>
+	</view>
+</template>
+
+<script>
+	import MyTest from '../../components/mpvueGestureLock/index.vue';
+
+	export default {
+		components: {
+			'MyTest': MyTest
+		},
+		data() {
+			return {
+				password: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+			};
+		},
+		methods: {
+			endfuntion: function(e) {
+				console.log(e)
+			}
+		}
+	}
+</script>
+
+<style>
+    body,
+    page {
+		padding: 0upx;
+		margin: 0upx;
+    }
+</style>
